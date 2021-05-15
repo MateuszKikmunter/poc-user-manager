9f35a2bb369202307c4a8962c101ea3c6b543dad
m.kikmunter <M.Kikmunter@gmail.com>
2021-05-15 20:43:15 +0100

    (feat) added user route with validator

---
+++ src/middleware/user.validator.ts
@@ -0,0 +1,39 @@
+//libs imports
+import { NextFunction, Request, Response } from 'express';
+import { validationResult, check } from 'express-validator';
+
+//local imports
+import { HttpCode } from '../utils/http.code';
+
+
+/**
+ * Validation rules applied for user.
+ */
+export const loginUserRules = [
+    check('email').isEmail().withMessage('Please enter a valid email.'),
+    check('email').notEmpty().withMessage('Email is required.'),    
+    check('password').notEmpty().withMessage('Password is required.'),
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long.')
+];
+
+export const createOrUpdateUserRules = [
+    ...loginUserRules,
+    check('name').notEmpty().withMessage('Name is required.')
+];
+
+/**
+ * * Validates details provided by the user when creating or updating user.
+ * * Returns HTTP 422 if invalid details provided.
+ * @param req request
+ * @param res response
+ * @param next next function
+ */
+export const validateUserRequest = (req: Request, res: Response, next: NextFunction): void | Response => {
+    
+    const validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()) {
+        return res.status(HttpCode.UNPROCESSABLE_ENTITY).json({ errors: validationErrors.array()?.map(err => err.msg) });
+    }
+
+    return next();
+}
--- src/routes/user.rotue.ts
+++ src/routes/user.rotue.ts
@@ -1,6 +1,26 @@
 //local imports
 import { Route } from './../shared/interfaces/route';
+import { UserController } from './../controllers/user.controller';
+import { createOrUpdateUserRules, validateUserRequest } from '../middleware/user.validator';
+
 
 export class UserRoute extends Route {
 
+    private userURL = '/api/users';
+    private _userController: UserController;
+
+    constructor() {
+        super();        
+        this._userController = new UserController();
+        this.initRoutes();
+    }
+
+    private initRoutes(): void {
+        //in real life those routes would be secured by some kind of middleware, e.g. JWT middleware checking if valid token provided
+        this.router.get(`${this.userURL}/get-all`, this._userController.getAll);
+        this.router.get(`${this.userURL}/:id`, this._userController.getUser);
+        this.router.delete(`${this.userURL}/:id`, this._userController.deleteUser);
+        this.router.put(`${this.userURL}/:id`, createOrUpdateUserRules, [validateUserRequest, this._userController.updateUser]);
+        this.router.post(`${this.userURL}/create`, createOrUpdateUserRules, [validateUserRequest, this._userController.createUser]);
+    }
 }
