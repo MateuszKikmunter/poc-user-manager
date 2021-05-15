3c1cad49cf944a7fff0f2cf784eaa281d17af5b3
m.kikmunter <M.Kikmunter@gmail.com>
2021-05-15 22:13:42 +0100

    (fix) simplefied auth route-identic to user route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,28 +1,23 @@
 //libs imports
 import { loginUserRules, validateUserRequest } from '../middleware/user.validator';
-import { Router } from 'express';
 
 //local imports
 import { Route } from '../shared/abstract/route';
 import { AuthController } from '../controllers/auth.controller';
 
 
-export class  AuthRoute implements Route {
+export class  AuthRoute extends Route {
 
     private authURL = '/api/auth';
     //we could inject those dependencies with depedency injection container to make it easier to test and to decouple it
     private authController: AuthController;
-    private _router: Router;
-    get router(): Router {
-        return this._router;
-    }
     constructor() {
-        this.authController = new AuthController();        
-        this._router = Router();
+        super();
+        this.authController = new AuthController(); 
         this.initRoutes();
     }
 
     private initRoutes(): void {        
-        this._router.post(`${this.authURL}/login`, loginUserRules,  [validateUserRequest, this.authController.login]);
+        this.router.post(`${this.authURL}/login`, loginUserRules, [validateUserRequest, this.authController.login]);
     }
 }
